dc199fffc28c0ed5cf8ef5ec091470c6e1aaf7b8
yasnling001 <1964074623@qq.com>
2020-03-18 17:15:10 +0800

    leetcode

---
+++ leetcode/LeetCode_56.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+//合并区间
+public class LeetCode_56 {
+    private static class Interval {
+        int start;
+        int end;
+        Interval(int[] interval) {
+            this.start = interval[0];
+            this.end = interval[1];
+        }
+
+        int[] toArray() {
+            return new int[]{this.start, this.end};
+        }
+    }
+
+    private static class IntervalComparator implements Comparator<Interval> {
+
+        @Override
+        public int compare(Interval a, Interval b) {
+            return Integer.compare(a.start, b.start);
+        }
+    }
+
+    public int[][] merge(int[][] intervals) {
+        List<Interval> intervalsList = new LinkedList<>();
+        for (int[] interval : intervals) {
+            intervalsList.add(new Interval(interval));
+        }
+        intervalsList.sort(new IntervalComparator());
+
+        LinkedList<Interval> merged = new LinkedList<>();
+        for (Interval interval : intervalsList) {
+
+            if (merged.isEmpty() || merged.getLast().end < interval.start) {
+                merged.add(interval);
+            }
+
+            else {
+                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+            }
+        }
+
+        int i = 0;
+        int[][] result = new int[merged.size()][2];
+        for (Interval mergedInterval : merged) {
+            result[i] = mergedInterval.toArray();
+            i++;
+        }
+        return result;
+    }
+}
---
+++ leetcode/SemaphoreDemo.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+//使用Semaphore交替打印100
+public class SemaphoreDemo {
+    static int a=0;
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(1);
+     //   ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+    }
+}
