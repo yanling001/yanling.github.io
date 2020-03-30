590adbedda3d1d9d6d789ef7db1d62577431b0c3
yasnling001 <1964074623@qq.com>
2020-03-30 21:40:23 +0800

    rpc

---
+++ wheel/RPC/src/Server.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+/**
+ * @author: guangxush
+ * @create: 2019/05/18
+ */
+public interface Server {
+
+    void start() throws IOException;
+
+    void register(Class serviceInterface, Class impl);
+
+    boolean isRunning();
+
+    int getPort();
+
+    void stop();
+}
