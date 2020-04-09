5d36ab09765851edbea92b64958d8b4bc362b6b6
yasnling001 <1964074623@qq.com>
2020-04-09 15:48:09 +0800

    leetcode

---
+++ leetcode/LeetCode_407.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Random;
+
+//rand7 实现 rand10
+public class LeetCode_407 {
+    public int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }
+    public int rand7(){
+        Random random=new Random();
+       return random.nextInt(7);
+    }
+}
