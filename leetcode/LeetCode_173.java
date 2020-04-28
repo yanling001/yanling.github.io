82947eec97791de4aaa7b7eb674d964593fb2512
yasnling001 <1964074623@qq.com>
2020-04-28 23:38:46 +0800

    leetcode

---
+++ leetcode/LeetCode_173.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class LeetCode_173 {
+    static  Stack<TreeNode> stack=new Stack<>();
+    public LeetCode_173(TreeNode root) {
+          make(root);
+    }
+
+    private void make(TreeNode root) {
+      while (root!=null){
+          stack.push(root);
+          root=root.left;
+      }
+    }
+
+
+    /** @return the next smallest number */
+    public int next() {
+     TreeNode temp=stack.pop();
+     if (temp.right!=null){
+         make(temp.right);
+     }
+     return  temp.val;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+      return !stack.isEmpty();
+    }
+}
